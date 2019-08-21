f81d059d4c5a6d7321c65d6f7c517ee226a8fe9b
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-08-21 20:36:38 +1200

    server

--- server.js
+++ server.js
@@ -1,25 +1,55 @@
+
 const express = require('express');
 const app = express();
 const port = 3000;
-const path = require('path');
 
 const allProducts = require('./data/products');
 
 app.use(function(req, res, next){
-    console.log(`${req.method} request for ${req.url}`);
-    next();
+  console.log(`${req.method} request for ${req.url}`);
+  next();
 });
 
 app.get('/', function(req, res){
-    res.send('Welcome to our Products API. Use endpoints to filter out the data');
+  res.send('Welcome to our Products API. Use endpoints to filter out the data');
 });
 
 app.get('/all', function(req, res) {
-  res.json(data);
+  res.send(allProducts);
 });
 
-app.get('/name', (req, res) => res.send('Instock'));
-app.get('/price', (req, res) => res.send('min price'));
+app.get('/products/:id', function(req, res) {
+  const oneProduct = req.params.id;
+  let filteredData = [];
+  for (var i = 0; i < allProducts.length; i++) {
+    if (allProducts[i].id.toString() === oneProduct) {
+      filteredData.push(allProducts[i]);
+    }
+  }
+  res.send(filteredData)
+});
+
+app.get('/products/edit/:id', function(req, res) {
+  const oneProduct = req.params.id;
+  let filteredData = [];
+  for (var i = 0; i < allProducts.length; i++) {
+    if (allProducts[i].id.toString() === oneProduct) {
+      filteredData.push(allProducts[i]);
+    }
+  }
+  res.send(filteredData)
+});
+
+app.get('/products/delete/:id', function(req, res) {
+  const oneProduct = req.params.id;
+  let filteredData = [];
+  for (var i = 0; i < allProducts.length; i++) {
+    if (allProducts[i].id.toString() === oneProduct) {
+      filteredData.push(allProducts[i]);
+    }
+  }
+  res.send(filteredData)
+});
 
 app.listen(port, () => {
     console.clear();
